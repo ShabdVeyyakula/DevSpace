4f31f709ccc6960581b3c215deeb332350a0cb40
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2020-03-28 14:23:19 -0700

    node package for twilio

--- assets/js/send-message.js
+++ assets/js/send-message.js
@@ -4,11 +4,20 @@ const authToken = '37088b69ccb5bd75796ec3a107c4ad30'
 const client = require('twilio')(accountSid, authToken)
 
 
-client.messages.create({
-    to: '+15103780762',
-    from: '+12093964767',
-    body: 'Hello kush this is a twilio message'
-    
-})
 
-.then((message) => console.log(message.sid));
+document.getElementById('signupp').onclick = function() {
+    var judges = '+1' + document.getElementById('thenumber').value
+
+    client.messages.create({
+        to: judges,
+        from: '+12093964767',
+        body: 'Hello kush this is a twilio message'
+        
+    })
+    
+    .then((message) => console.log(message.sid));
+
+
+}
+
+
